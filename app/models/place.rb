5ad31bd55ce9be970362f69171742e56dd31ff4e
Denise Kelsey <dkelsey@bellsouth.net>
2017-07-31 19:10:44 +0000

    additional validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
-  
+  validates :name, presence: true, length: { minimum: 3, too_short: "Minimum %{count} characters required." }, uniqueness: { case_sensitive: false }
+  validates :address, presence: true, length: { minimum: 3, too_short: "Minimum %{count} characters required." } 
+  validates :description, presence: true, length: { minimum: 3, too_short: "Minimum %{count} characters required." } 
 end
