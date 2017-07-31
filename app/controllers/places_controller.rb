a562466fa23d570b479b03566e5de09e3b3462bb
Denise Kelsey <dkelsey@bellsouth.net>
2017-07-31 18:06:56 +0000

    added validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,32 +1,41 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 
   def index
     @places = Place.page(params[:page]).per(5)
   end
 
+
   def new
     @place = Place.new
   end
 
+
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+  
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   def show
     @place = Place.find(params[:id])
   end
 
+
   def edit
     @place = Place.find(params[:id])
 
     if @place.user != current_user
-      return render text: 'Access Not Allowed', status: :forbidden
+      return render file: Rails.root.join('public', 'not_found.html.erb'), status: :not_found
     end
-        
   end
+
 
   def update
     @place = Place.find(params[:id])
@@ -36,23 +45,32 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to place_path
-
+    if @place.valid?
+       redirect_to place_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
 
   end
+
 
   def destroy
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Access Not Allowed', status: :forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
+
 
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 
 end
 
