d25d6c1748e5db6b35e24a06b09ce9d41d2692df
Denise Kelsey <dkelsey@bellsouth.net>
2017-08-23 03:28:19 +0000

    almost done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,14 +2,17 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
   after_create :send_comment_email
+
+  validates :message, presence: true, length: { maximum: 200, minimum: 3 }
  
 
   RATINGS = {
-    "1star.jpg":  '1_star',
+    "0stars.png": '0_stars',
+    "1star.png":  '1_star',
     "2stars.png": '2_stars',
     "3stars.png": '3_stars',
-    "4stars.jpg": '4_stars',
-    "5stars.png": '5_stars'
+    "4stars.png": '4_stars',
+    "5stars.png": '5_stars' 
   }
 
   def rating_image
